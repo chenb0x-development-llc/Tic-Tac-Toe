8d002b7a1ddcad5f6ed87674d6dac6d643686f12
chenb0x <artofmorellc@gmail.com>
2020-11-16 15:46:55 -0800

    Replayability

--- tictactoe.py
+++ tictactoe.py
@@ -15,68 +15,71 @@ def printBoard(board):
     print('-+-+-')
     print(f"{board['1']}|{board['2']}|{board['3']}")
 
-
-gameBoard = {'7': ' ', '8': ' ', '9': ' ',
-             '4': ' ', '5': ' ', '6': ' ',
-             '1': ' ', '2': ' ', '3': ' '}
-
-turn = 'X'
 count = 0
+play = 'y'
 
-for i in range(10):
-    printBoard(gameBoard)
-    move = input(f"It's your turn, {turn}. Move to which place?")
-    print(move)
+while play == "y":
+    gameBoard = {'7': ' ', '8': ' ', '9': ' ',
+                 '4': ' ', '5': ' ', '6': ' ',
+                 '1': ' ', '2': ' ', '3': ' '}
+    turn = 'X'
+    for i in range(10):
+        printBoard(gameBoard)
+        move = input(f"It's your turn, {turn}. Move to which place?")
+        print(move)
 
-    try:
-        if gameBoard[move] == ' ':
-            gameBoard[move] = turn
-            count += 1
-        else:
-            print("That spot is taken.")
+        try:
+            if gameBoard[move] == ' ':
+                gameBoard[move] = turn
+                count += 1
+            else:
+                print("That spot is taken.")
+                continue
+        except KeyError:
+            print("That is not a valid space.")
             continue
-    except KeyError:
-        print("That is not a valid space.")
-        continue
 
-    if count >= 5:
-        if gameBoard['7'] == gameBoard['8'] == gameBoard['9'] != ' ': #across top check
-            print("\nGame Over\n")
-            print(f"****** {turn} won! ******")
-            break
-        if gameBoard['4'] == gameBoard['5'] == gameBoard['6'] != ' ': #across middle check
-            print("\nGame Over\n")
-            print(f"****** {turn} won! ******")
-            break
-        if gameBoard['1'] == gameBoard['2'] == gameBoard['3'] != ' ': #across bottom check
-            print("\nGame Over\n")
-            print(f"****** {turn} won! ******")
-            break
-        if gameBoard['1'] == gameBoard['4'] == gameBoard['7'] != ' ': # left check
-            print("\nGame Over\n")
-            print(f"****** {turn} won! ******")
-            break
-        if gameBoard['2'] == gameBoard['5'] == gameBoard['8'] != ' ': # center check
-            print("\nGame Over\n")
-            print(f"****** {turn} won! ******")
-            break
-        if gameBoard['3'] == gameBoard['6'] == gameBoard['9'] != ' ': # right check
-            print("\nGame Over\n")
-            print(f"****** {turn} won! ******")
-            break
-        if gameBoard['1'] == gameBoard['5'] == gameBoard['9'] != ' ': # diagonal
-            print("\nGame Over\n")
-            print(f"****** {turn} won! ******")
-            break
-        if gameBoard['7'] == gameBoard['5'] == gameBoard['3'] != ' ': # diagonal
-            print("\nGame Over\n")
-            print(f"****** {turn} won! ******")
-            break
+        if count >= 5:
+            if gameBoard['7'] == gameBoard['8'] == gameBoard['9'] != ' ': #across top check
+                print("\nGame Over\n")
+                print(f"****** {turn} won! ******")
+                break
+            if gameBoard['4'] == gameBoard['5'] == gameBoard['6'] != ' ': #across middle check
+                print("\nGame Over\n")
+                print(f"****** {turn} won! ******")
+                break
+            if gameBoard['1'] == gameBoard['2'] == gameBoard['3'] != ' ': #across bottom check
+                print("\nGame Over\n")
+                print(f"****** {turn} won! ******")
+                break
+            if gameBoard['1'] == gameBoard['4'] == gameBoard['7'] != ' ': # left check
+                print("\nGame Over\n")
+                print(f"****** {turn} won! ******")
+                break
+            if gameBoard['2'] == gameBoard['5'] == gameBoard['8'] != ' ': # center check
+                print("\nGame Over\n")
+                print(f"****** {turn} won! ******")
+                break
+            if gameBoard['3'] == gameBoard['6'] == gameBoard['9'] != ' ': # right check
+                print("\nGame Over\n")
+                print(f"****** {turn} won! ******")
+                break
+            if gameBoard['1'] == gameBoard['5'] == gameBoard['9'] != ' ': # diagonal
+                print("\nGame Over\n")
+                print(f"****** {turn} won! ******")
+                break
+            if gameBoard['7'] == gameBoard['5'] == gameBoard['3'] != ' ': # diagonal
+                print("\nGame Over\n")
+                print(f"****** {turn} won! ******")
+                break
         
 
-    if turn == 'X':
-        turn = 'O'
-    else:
-        turn = 'X'
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'
 
-printBoard(gameBoard)  
+    printBoard(gameBoard)
+    play = input("Would you like to play again? (Y)es/(N)o")
+    play = play.lower()
+  
